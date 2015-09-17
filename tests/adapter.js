ea80b9d1c75d16fa16f015053cbc262c43d81a7e
Pedro Teixeira <i@pgte.me>
2015-09-17 09:09:30 +0100

    can post

--- tests/adapter.js
+++ tests/adapter.js
@@ -53,7 +53,7 @@ describe('Adapter', function() {
 
     stream.once('data', function(d) {
       expect(d).to.deep.equal([seq,"dbname","_bulkDocs",[{"docs":[{"_id":"id","a":1,"b":2}]},{"new_edits":true}]]);
-      stream.write([seq, [null, {ok: true, _id: 'id', _rev: 1}]]);
+      stream.write([seq, [null, {ok: true, id: 'id', rev: 1}]]);
     });
 
     remoteDB.put({_id: 'id', a:1,b:2}, function(err, result) {
@@ -61,7 +61,7 @@ describe('Adapter', function() {
         done(err);
       }
       else {
-        expect(result).to.deep.equal({ok: true, _id: 'id', _rev: 1});
+        expect(result).to.deep.equal({ok: true, id: 'id', rev: 1});
         done();
       }
     });
@@ -71,7 +71,7 @@ describe('Adapter', function() {
   it('can be used to get a doc', function(done) {
     var seq = sequence();
     var stream = remote.stream();
-    stream.on('data', function(d) {
+    stream.once('data', function(d) {
       expect(d).to.deep.equal([seq, 'dbname', 'get', ['alice']]);
       stream.write([seq, [null, {ok: true}]]);
     });
@@ -86,42 +86,26 @@ describe('Adapter', function() {
     });
   });
 
+  it('can be used to post a doc', function(done) {
+    var seq = sequence();
+    var stream = remote.stream();
+    stream.once('data', function(d) {
+      expect(d).to.deep.equal([seq, 'dbname', '_bulkDocs', [{"docs":[{"_id":"id","a":2,"b":3}]},{"new_edits":true}]]);
+      stream.write([seq, [null, {ok: true, id: 'id', rev: 1}]]);
+    });
+    remoteDB.post({"_id":"id","a":2,"b":3}, function(err, result) {
+      if (err) {
+        done(err);
+      }
+      else {
+        expect(result).to.deep.equal({ok: true, id: 'id', rev: 1});
+        done();
+      }
+    });
+  });
 
-
-  //   server.inject('/cart', function(res) {
-  //     expect(res.statusCode).to.be.equal(500);
-  //     mock.done();
-  //     done();
-  //   });
-  // });
 });
 
-// var Reconnect = require('reconnect-core');
-
-// var reconnect = Reconnect(function(options) {
-//   return net.connect(options.port, options.host);
-// });
-
-// var options = {
-//   port: 80,
-//   host: '127.0.0.1'
-// };
-
-// var re = reconnect(options, function(stream) {
-
-//   var remote = Remote();
-
-//   var remoteDB = new PouchDB({
-//     adapter: 'remote',
-//     name: 'mydb',
-//     remote: remote
-//   });
-
-//   stream.pipe(remote).pipe(stream);
-
-//   // use remoteDB
-
-// });
 
 function xit() {}
 
